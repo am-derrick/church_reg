f808232bede23cc5d2849d98edbe0afa5b26c915
Derrick Ampire <ampire90@gmail.com>
2024-10-05 11:37:53 +0300

    fix: removed unused imports and variables

--- members/models.py
+++ members/models.py
@@ -1,7 +1,7 @@
-# from django.contrib.auth.models import User
 from django.db import models
 
 class Registration(models.Model):
+    """Registration form input fields model"""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10)
@@ -12,10 +12,6 @@ class Registration(models.Model):
     consent = models.BooleanField(default=False)
 
     # To-Do: Add field for occupation
-
-    # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # is_first_timer = models.BooleanField(default=True)
-    # last_visit = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
--- members/views.py
+++ members/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-# from .models import MemberProfile
 from .forms import RegistrationForm
 
 

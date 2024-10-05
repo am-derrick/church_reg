a33a9269bd475d261cb70850846ea9bda57f9b1b
Derrick Ampire <ampire90@gmail.com>
2024-10-05 12:36:17 +0300

    fix: added Meta class in Registration form

--- members/forms.py
+++ members/forms.py
@@ -3,12 +3,13 @@ from .models import Registration
 
 class RegistrationForm(forms.ModelForm):
     """Registration form utilising Django forms"""
-    model = Registration
-    fields = ['first_name', 'last_name', 'gender', 'phone_number', 'residence',
-              'is_student', 'is_first_time', 'consent']
-    widgets = {
-        'gender': forms.RadioSelect(choices=[('Male', 'Male'), ('Female', 'Female')]),
-        'is_student': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
-        'is_first_time': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
-        'consent': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
-    }
+    class Meta:
+        model = Registration
+        fields = ['first_name', 'last_name', 'gender', 'phone_number', 'residence',
+                'is_student', 'is_first_time', 'consent']
+        widgets = {
+            'gender': forms.RadioSelect(choices=[('Male', 'Male'), ('Female', 'Female')]),
+            'is_student': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+            'is_first_time': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+            'consent': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+        }

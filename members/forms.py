c2ae4bd3e0a51e68ca149a25a7feaa97bf84e7bc
Derrick Ampire <ampire90@gmail.com>
2024-10-05 11:29:58 +0300

    add: form for regsitration logic

---
+++ members/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Registration
+
+class RegistrationForm(forms.ModelForm):
+    """Registration form utilising Django forms"""
+    model = Registration
+    fields = ['first_name', 'last_name', 'gender', 'phone_number', 'residence',
+              'is_student', 'is_first_time', 'consent']
+    widgets = {
+        'gender': forms.RadioSelect(choices=[('Male', 'Male'), ('Female', 'Female')]),
+        'is_student': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+        'is_first_time': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+        'consent': forms.RadioSelect(choices=[(True, 'Yes'), (False, 'No')]),
+    }

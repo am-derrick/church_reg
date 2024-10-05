4f225344484f7a649d45b22cc7a0627e2fb7863b
Derrick Ampire <ampire90@gmail.com>
2024-10-05 11:35:42 +0300

    modified: created  and  pages views

--- members/views.py
+++ members/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from .models import MemberProfile
+# from .models import MemberProfile
+from .forms import RegistrationForm
 
 
 def registration_view(request):
+    """Registration form view: displays registration form"""
     if request.method == 'POST':
-        # to-do: handle form submission logic
-        pass
-    
-    return render(request, 'registration.html')
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('thank_you')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration.html', {'form': form})
+
+def thank_you_view(request):
+    """Thank you page view, displayed after registration"""
+    return render(request, 'thank_you.html')

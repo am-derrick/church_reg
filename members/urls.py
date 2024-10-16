f66c62df391b624a9bd550003777b781b2c758e9
Derrick Ampire <ampire90@gmail.com>
2024-10-16 16:05:32 +0300

    add: path url for welcome page

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.registration_view, name='register')
+    path('register/', views.registration_view, name='register'),
+    path('welcome/<str:first_name>/', views.welcome_view, name='welcome')
 ]

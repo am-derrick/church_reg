65465bc58db2d5f4a833b6e0b3ddcb38182bebec
Derrick Ampire <ampire90@gmail.com>
2024-10-16 22:46:44 +0300

    refactor: converted radio fields from boolean to choice fields

--- members/models.py
+++ members/models.py
@@ -4,15 +4,15 @@ class Registration(models.Model):
     """Registration form input fields model"""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    gender = models.CharField(max_length=10)
+    gender = models.CharField(max_length=6, choices=[('Male', 'Male'), ('Female', 'Female')])
     phone_number = models.CharField(max_length=15)
     residence = models.CharField(max_length=255)
-    is_student = models.BooleanField(default=False)
-    institution_name = models.CharField(max_length=255)
-    institution_location = models.CharField(max_length=255)
-    occupation = models.CharField(max_length=255)
-    is_first_time = models.BooleanField(default=False)
-    consent = models.BooleanField(default=False)
+    is_student = models.CharField(max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
+    institution_name = models.CharField(max_length=255, blank=True, null=True)
+    institution_location = models.CharField(max_length=255, blank=True, null=True)
+    occupation = models.CharField(max_length=255, blank=True, null=True)
+    is_first_time = models.CharField(max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
+    consent = models.CharField(max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"

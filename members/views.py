c8612fe52a31a2f5fb8a7fbc403a36f61fa38d16
Derrick Ampire <ampire90@gmail.com>
2024-10-16 16:03:59 +0300

    modified:  parameter passed to

--- members/views.py
+++ members/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 
-
 def registration_view(request):
     """Registration form view: displays registration form"""
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('welcome')
+            registration = form.save()
+            return redirect('welcome', first_name=registration.first_name)
     else:
         form = RegistrationForm()
-
     return render(request, 'registration.html', {'form': form})
 
-def welcome_view(request):
+def welcome_view(request, first_name):
     """Thank you page view, displayed after registration"""
-    return render(request, 'welcome.html')
+    return render(request, 'welcome.html', {'first_name': first_name})

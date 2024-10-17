ce38f278afdaac9a5a6f15c572d6c4db4aade523
Derrick Ampire <ampire90@gmail.com>
2024-10-17 11:19:27 +0300

    fix: added localhost and known ip to allowed_hosts

--- church_registration/settings.py
+++ church_registration/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^&-q%f%&8u@)*c&!lz*e_!!hfwt5$f@xic*#n*r@d46^(+z2$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.100.91", "127.0.0.1"]
 
 
 # Application definition

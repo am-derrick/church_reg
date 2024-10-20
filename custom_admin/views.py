cec3dab41a84045bfc2cc20b5a7e7bde91798191
Derrick Ampire <ampire90@gmail.com>
2024-10-20 23:44:14 +0300

    add: CRUD functionality created for super_admin

--- custom_admin/views.py
+++ custom_admin/views.py
@@ -1,12 +1,14 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
-from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth import get_user_model
+from django.contrib.admin.decorators import user_passes_test
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from members.models import Registration
 
+
+User = get_user_model()
 
 def login_view(request):
     """login view"""
@@ -32,3 +34,48 @@ def admin_dashboard(request):
     """view for the admin dashboard"""
     registrations = Registration.objects.all().order_by('-created_at')
     return render(request, 'custom_admin/admin_dashboard.html', {'registrations': registrations})
+
+def is_super_admin(user):
+    return user.is_authenticated and user.is_super_admin()
+
+@user_passes_test(is_super_admin)
+def user_list(request):
+    """displays list for all users(admins)"""
+    users = User.objects.all().order_by('-date_joined')
+    return render(request, 'custom_admin/user_list.html', {'users': users})
+
+@user_passes_test(is_super_admin)
+def user_create(request):
+    """super user creates admins"""
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User created successfully.')
+            return redirect('user_list')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'custom_admin/user_form.html', {'form': form})
+
+@user_passes_test(is_super_admin)
+def user_edit(request, user_id):
+    """super user edits users"""
+    user = get_object_or_404(User, id=user_id)
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'User updates successfully.')
+            return redirect('user_list')
+    else:
+        form = CustomUserChangeForm(instance=user)
+    return render(request, 'custom_admin/user_form.html', {'form': form})
+
+@user_passes_test(is_super_admin)
+def user_deactivate(request, user_id):
+    """super user deletes/deactivates other users"""
+    user = get_object_or_404(User, id=user_id)
+    user.is_active = False
+    user.save()
+    messages.success(request, 'User deactivated succesfully.')
+    return redirect('user_list')

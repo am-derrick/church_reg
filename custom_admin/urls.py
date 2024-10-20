c1f75efa6d12297d3fff555e714106a01e19f06b
Derrick Ampire <ampire90@gmail.com>
2024-10-20 23:49:40 +0300

    modified: urls for super admin CRUD functionality added

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.admin_dashboard, name='admin_dashboard'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/create/', views.user_create, name='user_create'),
+    path('users/<int:user_id>/edit/', views.user_edit, name='user_edit'),
+    path('users/<int:user_id>/deactivate/', views.user_deactivate, name='user_deactivate'),
 ]

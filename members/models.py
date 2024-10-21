1a0bd4a5b1d018b9566dee3a247c00f107b314a8
Yahweh's Delight <intricatesyllable@gmail.com>
2024-10-21 04:05:03 +0300

    Pylint successful

--- members/models.py
+++ members/models.py
@@ -1,19 +1,29 @@
+"""
+This module contains the models for the members of the church,
+including registration details and other relevant data structures.
+"""
 from django.db import models
+
 
 class Registration(models.Model):
     """Registration form input fields model"""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    gender = models.CharField(max_length=6, choices=[('Male', 'Male'), ('Female', 'Female')])
+    gender = models.CharField(max_length=6, choices=[
+                              ('Male', 'Male'), ('Female', 'Female')])
     phone_number = models.CharField(max_length=12, unique=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
     residence = models.CharField(max_length=255)
-    is_student = models.CharField(max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
+    is_student = models.CharField(
+        max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
     institution_name = models.CharField(max_length=255, blank=True, null=True)
-    institution_location = models.CharField(max_length=255, blank=True, null=True)
+    institution_location = models.CharField(
+        max_length=255, blank=True, null=True)
     occupation = models.CharField(max_length=255, blank=True, null=True)
-    is_first_time = models.CharField(max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
-    consent = models.CharField(max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
-    
+    is_first_time = models.CharField(
+        max_length=3, choices=[('Yes', 'Yes'), ('No', 'No')])
+    consent = models.CharField(max_length=3, choices=[
+                               ('Yes', 'Yes'), ('No', 'No')])
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"

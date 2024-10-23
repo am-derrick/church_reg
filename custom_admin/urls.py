f4d5010d538270738daa332df334771b1844d66f
Derrick Ampire <ampire90@gmail.com>
2024-10-23 16:57:12 +0300

    add: export-registration url

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import export
 
 urlpatterns = [
     path('', views.admin_dashboard, name='admin_dashboard'),
@@ -9,4 +10,5 @@ urlpatterns = [
     path('users/create/', views.user_create, name='user_create'),
     path('users/<int:user_id>/edit/', views.user_edit, name='user_edit'),
     path('users/<int:user_id>/deactivate/', views.user_deactivate, name='user_deactivate'),
+    path('export-registration/', export.export_registrations_csv, name='export_registrations_csv')
 ]

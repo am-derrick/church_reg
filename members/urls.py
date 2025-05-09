c7e177b4866e021719e64f6494fa3ecb5546de24
Derrick Ampire <ampire90@gmail.com>
2025-05-09 14:58:05 +0300

    modified: renamed  to

--- members/urls.py
+++ members/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("register/confirm/", views.registration_confirm, name="registration_confirm"),
     path("register/submit/", views.registration_submit, name="registration_submit"),
     path("welcome/<str:first_name>/", views.welcome_view, name="welcome"),
-
-    path("events/", views.events_register_view, name="events_register_view"),
+    path("events/", views.events_register_view, name="events"),
     path("events/submit/", views.events_submit, name="events_submit"),
+    path("events/confirm/", views.events_confirm, name="events_confirm"),
 ]
